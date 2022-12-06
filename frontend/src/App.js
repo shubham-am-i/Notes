93d9eb66f48807a3f053cbe7d861673b46f60252
shubham-am-i <patilshubham2k21@gmail.com>
2022-12-06 17:22:07 +0530

    initial project setup

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import CreateNote from './components/CreateNote'
+import Header from './components/Header'
+
+function App() {
+  return (
+    <div className='App'>
+      <Header />
+      <CreateNote />
+    </div>
+  )
+}
+
+export default App
